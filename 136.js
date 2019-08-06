c7db61ae3d6e8712683ff1bf362b93bfd739fecc
ches4117 <ches4117@gmail.com>
2019-08-06 21:29:35 +0800

    Update 136 file solution

--- 136.js
+++ 136.js
@@ -3,20 +3,11 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-    let myMap = new Map();
-    myMap.set(nums[0], '1')
-    let length = nums.length
-    if (length > 1) {
-        for (let i = 1; i < nums.length; i++) {
-            if (myMap.get(nums[i])) {
-                myMap.delete(nums[i])
-            } else {
-                myMap.set(nums[i], '1')
-            }
-        }
-    }
-
-    for (var key of myMap.keys()) {
-        return key
-    }
+    answer = 0
+    nums.forEach((num) => {
+        answer ^= num
+    })
+    return answer
 };
+
+console.log(singleNumber([4, 1, 2, 1, 2]))
